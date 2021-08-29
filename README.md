108500ffd10c6af75c6ae50664b27e4773f1f2a2
Artyom Kouznetsov <art.kouznetsov@gmail.com>
2021-08-29 17:07:02 +0200

    v1.01

---
+++ README.md
@@ -0,0 +1,23 @@
+# ArtyomDevLanding
+yarn add --dev @nuxt/image
+
+yarn add -D sass sass-loader@10 fibers
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+## Special Directories
---
+++ nuxt.config.js
@@ -0,0 +1,67 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Artyom Kouznetsov - Web Developer',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Artyom Kouznetsov Web Developer Landing Page' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Nunito&display=swap"}
+    ],
+    script: [
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/gsap.min.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/ScrollTrigger.min.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/TextPlugin.min.js' },
+      { src: 'https://kit.fontawesome.com/57ce9bd511.js', crossorigin: 'anonymous' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    // '~/assets/css/reset.css',
+    // '~/assets/css/responsive.css',
+    // '~/assets/css/fonts.css',
+    // '~/assets/css/animation.css',
+    // '~/assets/css/global.css',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // '@nuxtjs/fontawesome',
+    '@nuxt/image'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  transition: {
+    name: 'fade',
+    // <<< fade is the first part of the class we use in our animation.css file
+    mode: 'out-in'
+  }
+}
